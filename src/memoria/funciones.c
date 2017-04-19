1cc9c8f45535aea6f0cafd30c0f74e7489ed8c3a
dhbarrios <diego@rukkus.com>
2017-04-19 18:52:51 -0300

    Update funciones.c

--- src/memoria/funciones.c
+++ src/memoria/funciones.c
@@ -1,3 +1,6 @@
+/*
+Obtener cantidad de paginas por programa
+*/
 int cantidadPaginasPorPrograma(int iTamProg, int iTamPag){
 	// Obtengo la cantidad de paginas que debera tener el programa
 	int iCant = iTamProg/iTamPag;
@@ -5,4 +8,22 @@ int cantidadPaginasPorPrograma(int iTamProg, int iTamPag){
 		iCant++;
 	}
 	return iCant;
+}
+
+/*
+Inicializar la Memoria Principal
+*/
+void inicializarMemoriaPrincipal(int iCantPag, int iTamPag){
+
+	char *bits_bitMap = malloc(sizeof(char)*iCantPag);
+	int i;
+
+	logDebug("== Inicializando Memoria Principal: %d bytes\n", iCantPag*iTamPag);
+
+	for(i=0;i<iCantPag;i++){
+		bits_bitMap[i]=0;
+	}
+
+	memoria_principal.memoria = malloc(iCantPag*iTamPag);
+	memoria_principal.bitmap = bitarray_create(bits_bitMap,iCantPag);
 }
