91e44330ecb3349ecb5925679ec04aa2f5bb4867
dhbarrios <diego@rukkus.com>
2017-04-19 18:02:10 -0300

    Update mem.c

--- src/memoria/mem.c
+++ src/memoria/mem.c
@@ -1,1 +1,14 @@
+void inicializarMemoriaPrincipal(int iCantPag, int iTamPag){
 
+	char *bits_bitMap = malloc(sizeof(char)*iCantPag);
+	int i;
+
+	logDebug("Creando memoria principal de tamanio %d\n", iCantPag*iTamPag);
+
+	for(i=0;i<iCantPag;i++){
+		bits_bitMap[i]=0;
+	}
+
+	memoria_principal.memoria = malloc(iCantPag*iTamPag);
+	memoria_principal.bitmap = bitarray_create(bits_bitMap,iCantPag);
+}
