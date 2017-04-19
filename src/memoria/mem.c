f66895e3d8afab51f49b96610cd9310fe7abcb4a
dhbarrios <diego@rukkus.com>
2017-04-19 18:54:04 -0300

    Update mem.c

--- src/memoria/mem.c
+++ src/memoria/mem.c
@@ -1,14 +1,36 @@
-void inicializarMemoriaPrincipal(int iCantPag, int iTamPag){
+/*
+ ============================================================================
 
-	char *bits_bitMap = malloc(sizeof(char)*iCantPag);
-	int i;
+ ============================================================================
+ */
 
-	logDebug("== Inicializando Memoria Principal: %d bytes\n", iCantPag*iTamPag);
+#include "mem.h"
 
-	for(i=0;i<iCantPag;i++){
-		bits_bitMap[i]=0;
-	}
+int main(int argc, char *argv[]){
 
-	memoria_principal.memoria = malloc(iCantPag*iTamPag);
-	memoria_principal.bitmap = bitarray_create(bits_bitMap,iCantPag);
+	pthread_t hilo1;
+
+	//Leo la configuracion del archivo y lo guardo en una variable global (config)
+	configurar(argc > 1 ? argv[1] : NULL);
+
+	//creo el log
+	initLogMutex(config->log_file, config->log_program_name, config->log_print_console, log_level_from_string(config->log_level));
+
+	//Mutex para manejar el retardo de config
+	pthread_mutex_init(&retardo_mutex,NULL);
+
+	inicializarUMC(config);
+
+	//No uso mutex porque todavia no hay threads
+	continua=1;
+
+	pthread_create(&hilo1,NULL,(void*)handleClients,NULL);
+
+	handleComandos();
+
+	pthread_cancel(hilo1);
+
+	logDestroy();
+
+	return EXIT_SUCCESS;
 }
